9486755168253620ecb68e5b817ec7e01b71148c
NerdPersonThing <lukekronzer@gmail.com>
2016-12-26 14:25:44 +0000

    Add files via upload

--- buildV.js
+++ buildV.js
@@ -186,7 +186,7 @@ bot.on('message', (message) => {
         if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
             return message.channel.sendMessage('I don\'t have the proper permissions.');
         }
-        if(!muteMember.roles.has(mutedRole.id)) {
+        if(muteMember.roles.has(mutedRole.id)) {
             return message.channel.sendMessage('This member is already muted.');
         }
         muteMember.addRole(mutedRole);
